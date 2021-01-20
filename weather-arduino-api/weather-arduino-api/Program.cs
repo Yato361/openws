ac9a7564e98516df21ed8147af2ad0804f5bd791
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-20 09:17:38 +0100

    Added proxy configuration

--- weather-arduino-api/weather-arduino-api/Program.cs
+++ weather-arduino-api/weather-arduino-api/Program.cs
@@ -16,8 +16,11 @@ namespace weather_arduino_api
             CreateHostBuilder(args).Build().Run();
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        private static IHostBuilder CreateHostBuilder(string[] args) => 
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseUrls("http://127.0.0.1:5000");
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
