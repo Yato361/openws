1fdccd7e4bba3eb9a73c9e28137a6bb0a06d7a92
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-20 14:01:08 +0100

    Removed HTTPS redriction

--- weather-arduino-api/weather-arduino-api/Startup.cs
+++ weather-arduino-api/weather-arduino-api/Startup.cs
@@ -26,12 +26,12 @@ namespace weather_arduino_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
             services.Configure<ForwardedHeadersOptions>(options =>
             {
                 options.ForwardedHeaders =
                     ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
             });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,8 +46,6 @@ namespace weather_arduino_api
             {
                 app.UseForwardedHeaders();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
