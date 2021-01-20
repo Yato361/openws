e28ede57656512cfe1350125d3237986e7908414
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-20 13:21:47 +0100

    Added WAPI SQL test before starting the app

--- weather-arduino-api/weather-arduino-api/Program.cs
+++ weather-arduino-api/weather-arduino-api/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using weather_arduino_api.Database;
 
 namespace weather_arduino_api
 {
@@ -13,7 +15,21 @@ namespace weather_arduino_api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Debug.Debug.Log("Starting WAPI MySQL-Test...");
+            WeatherMySqlConnection weatherMySqlConnection = new WeatherMySqlConnection();
+            try
+            {
+                if (weatherMySqlConnection.GetConnection().State != ConnectionState.Closed)
+                {
+                    Debug.Debug.Log("MySQL-Test successfully carried out!");
+                    CreateHostBuilder(args).Build().Run();
+                }else
+                    Debug.Debug.Log("WAPI MySQL could not be connected. Program is stopping!");
+            }
+            catch (Exception e)
+            {
+                Debug.Debug.Log("WAPI MySQL could not be connected. Program is stopping! Exception: " + e.Message);
+            }
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) => 
